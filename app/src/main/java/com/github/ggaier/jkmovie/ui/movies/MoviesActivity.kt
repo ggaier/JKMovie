b52de03f14fcd8791ddf2f9f54676c9e1e219db7
ggaier <jwenbo52@gmail.com>
2017-06-08 13:52:13 +0800

    add movies activity and layout

--- app/src/main/java/com/github/ggaier/jkmovie/ui/movies/MoviesActivity.kt
+++ app/src/main/java/com/github/ggaier/jkmovie/ui/movies/MoviesActivity.kt
@@ -9,7 +9,9 @@ class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies)
+        
     }
+
 
 
 }
