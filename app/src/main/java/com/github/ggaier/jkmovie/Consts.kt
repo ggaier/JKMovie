7a9c82d4342a6adf2dfab477cbaf3c3178328329
ggaier <jwenbo52@gmail.com>
2017-06-13 14:11:27 +0800

    use slash of english version

--- app/src/main/java/com/github/ggaier/jkmovie/Consts.kt
+++ app/src/main/java/com/github/ggaier/jkmovie/Consts.kt
@@ -4,5 +4,5 @@ package com.github.ggaier.jkmovie
  * Created by ggaier
  * jwenbo52@gmail.com
  */
-val TMDB_BASE_URL = "https://api.themoviedb.org/3／"
-val TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500／"
+val TMDB_BASE_URL = "https://api.themoviedb.org/3/"
+val TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
