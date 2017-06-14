5e1f544ad9b1acec1c0fbdb2eb02050574b3a1b8
ggaier <jwenbo52@gmail.com>
2017-06-14 11:51:53 +0800

    add api key in network intercepter

--- app/src/main/java/com/github/ggaier/jkmovie/di/Injections.kt
+++ app/src/main/java/com/github/ggaier/jkmovie/di/Injections.kt
@@ -1,6 +1,7 @@
 package com.github.ggaier.jkmovie.di
 
 import com.github.ggaier.jkmovie.TMDB_BASE_URL
+import com.github.ggaier.jkmovie.api.ApiIntercepter
 import com.github.ggaier.jkmovie.api.ApiService
 import com.github.ggaier.jkmovie.data.MoviesRepository
 import com.github.ggaier.jkmovie.data.local.MoviesLocalDataSource
@@ -8,6 +9,7 @@ import com.github.ggaier.jkmovie.data.remote.MoviesRemoteDataSource
 import com.github.ggaier.jkmovie.ui.movies.MoviesPresenter
 import com.github.ggaier.jkmovie.ui.movies.MoviesPresenterIn
 import com.github.ggaier.jkmovie.ui.movies.MoviesView
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -27,12 +29,16 @@ class Injections {
         }
 
         @JvmStatic
-        fun apiService(): ApiService =
-                Retrofit.Builder().baseUrl(TMDB_BASE_URL)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                        .build()
-                        .create(ApiService::class.java)
+        fun apiService(): ApiService {
+            val builder = OkHttpClient.Builder()
+            builder.addNetworkInterceptor(ApiIntercepter())
+            return Retrofit.Builder().baseUrl(TMDB_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(builder.build())
+                    .build()
+                    .create(ApiService::class.java)
+        }
 
     }
 
