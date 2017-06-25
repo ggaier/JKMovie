e5a6bb52a7f37cc7278e427b76de909dcb78fa2f
ggaier <jwenbo52@gmail.com>
2017-06-26 00:16:10 +0800

    show load more view at the end of the position wrong solution

--- app/src/main/java/com/github/ggaier/jkmovie/ui/adapters/BaseAdapter.kt
+++ app/src/main/java/com/github/ggaier/jkmovie/ui/adapters/BaseAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.github.ggaier.jkmovie.R
 import com.github.ggaier.jkmovie.ui.adapters.BaseAdapter.BaseViewHolder
+import kotlinx.android.synthetic.main.list_item_load_more.view.*
 
 /**
  * Created by ggaier
@@ -17,6 +18,7 @@ class BaseAdapter<T>(
         val mDatas: MutableList<T> = mutableListOf()) : RecyclerView.Adapter<BaseViewHolder>() {
 
     companion object {
+        private val LOAD_MORE_THREADHOLD = 5
         private val VIEW_TYPE_LOAD_MORE = -0xff
         private val VIEW_TYPE_DEFAULT = -0xfe
     }
@@ -28,8 +30,25 @@ class BaseAdapter<T>(
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder?, position: Int) {
-
+        showLoadMore(holder, position)
     }
+
+    private var mLoadMoreComplete: Boolean = false
+    private var mLoadMoreFailed: Boolean = false
+
+    private fun showLoadMore(holder: BaseViewHolder?, position: Int) {
+        if (mDatas.isEmpty()) {
+            holder?.itemView?.load_more_progressbar?.visibility = View.GONE
+            return
+        }
+        if (position == mDatas.size - 1) {
+            holder?.itemView?.load_more_progressbar?.visibility = if (shouldShowLoadMore())
+                View.VISIBLE else View.GONE
+        }
+    }
+
+    private fun shouldShowLoadMore(): Boolean = !mLoadMoreComplete || mLoadMoreFailed
+
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BaseViewHolder {
         return BaseViewHolder(
