336fc1502da237492c4a85b3f6362b44f46e189f
ggaier <jwenbo52@gmail.com>
2017-07-07 00:25:10 +0800

    implement parcelable

--- app/src/main/java/com/github/ggaier/jkmovie/data/vo/Video.kt
+++ app/src/main/java/com/github/ggaier/jkmovie/data/vo/Video.kt
@@ -22,18 +22,54 @@ data class Video(val id: String,
                  @SerializedName("backdrop_path") private val backdropPath: String,
                  @SerializedName("adult") val isAdult: Boolean,
                  val overview: String,
-                 @SerializedName("release_date") val releaseDate: String):Parcelable {
-
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
-    }
-
-    override fun describeContents(): Int {
-    }
-
+                 @SerializedName("release_date") val releaseDate: String) : Parcelable {
     val realBackdropPath: String
         get() {
             return TMDB_IMAGE_BASE_URL.plus(backdropPath)
         }
+
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<Video> = object : Parcelable.Creator<Video> {
+            override fun createFromParcel(source: Parcel): Video = Video(source)
+            override fun newArray(size: Int): Array<Video?> = arrayOfNulls(size)
+        }
+    }
+
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readInt(),
+            1 == source.readInt(),
+            source.readFloat(),
+            source.readString(),
+            source.readFloat(),
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.createStringArray(),
+            source.readString(),
+            1 == source.readInt(),
+            source.readString(),
+            source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(id)
+        dest.writeInt(voteCount)
+        dest.writeInt((if (isVideo) 1 else 0))
+        dest.writeFloat(voteAverage)
+        dest.writeString(title)
+        dest.writeFloat(popularity)
+        dest.writeString(postPath)
+        dest.writeString(originalLanguage)
+        dest.writeString(originalTitle)
+        dest.writeStringArray(genreIds)
+        dest.writeString(backdropPath)
+        dest.writeInt((if (isAdult) 1 else 0))
+        dest.writeString(overview)
+        dest.writeString(releaseDate)
+    }
 }
 
 
